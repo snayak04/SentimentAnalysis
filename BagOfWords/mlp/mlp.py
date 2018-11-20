0f372c3a2ab8177ba681551ac71e3c923fa87fe8
lpatel16 <31355800+lpatel16@users.noreply.github.com>
2018-11-20 09:59:20 -0500

    Added pre-processing file (util.py)
    Refer to changes in mlp.py to account for new util.py file

--- BagOfWords/mlp/mlp.py
+++ BagOfWords/mlp/mlp.py
@@ -1,75 +1,19 @@
-import string
-from nltk.corpus import stopwords
-from string import punctuation
-from os import listdir
-from collections import Counter
+import util
 from numpy import array
 from keras.preprocessing.text import Tokenizer
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 
-
-#Open file in read-only and extract content into variable 'content'
-def loadFile(filename):
-    openFile = open(filename, 'r')
-    content = openFile.read()
-    openFile.close()
-    return content
-
-#Tokenize file
-def tokenizeFile(filename):
-    tokens = filename.split()                                   #remove whitespace
-    tokens = [x.strip(string.punctuation) for x in tokens]           #remove punctuation
-    tokens = [word for word in tokens if word.isalpha()]        #remove none alphabetic words
-    stopWords = set(stopwords.words('english'))                 #remove stop words
-    tokens = [word for word in tokens if not word in stopWords]
-    tokens = [word for word in tokens if len(word) > 1]         #remove 1-letter tokens
-    return tokens
-
-#Convert tokens to single strings for easier encoding
-def fileToLine(filename, vocab):
-    content = loadFile(filename)
-    tokens = tokenizeFile(content)
-    tokens = [word for word in tokens if word in vocab]
-    return ' '.join(tokens)
-
-#Load all reviews and start mapping words to counter
-def loadReviews(directory, vocab, is_train):
-    lines = list()
-    for filename in listdir(directory):
-        if filename.startswith('cv9') and is_train:
-            continue
-        if not filename.startswith('cv9') and not is_train:
-            continue
-        path = directory + '/' + filename
-        line = fileToLine(path, vocab)
-        lines.append(line)
-    return lines
-
-#Predict reviews based on MLP network
-def predictReview(review, vocab, tokenizer, model):
-
-    #Split review into words and filter based on current vocab
-    tokens = tokenizeFile(review)
-    tokens = [word for word in tokens if word in vocab]
-    lines = ' '.join(tokens)
-    encode = tokenizer.texts_to_matrix([lines], mode='freq')
-
-    #Predict review: 0 if positive, 1 if negative
-    y = model.predict(encode, verbose=0)
-    return round(y[0,0])
-
-
-### Main ###
+# Main
 vocabFile = 'vocab.txt'
-vocab = loadFile(vocabFile)
+vocab = util.loadFile(vocabFile)
 vocab = vocab.split()
 vocab = set(vocab)
 
 #Training set
-pos_reviews = loadReviews('data/pos', vocab, True)
-neg_reviews = loadReviews('data/neg', vocab, True)
+pos_reviews = util.loadReviews('data/pos', vocab, True)
+neg_reviews = util.loadReviews('data/neg', vocab, True)
 tokenizer = Tokenizer()
 total = pos_reviews+neg_reviews
 tokenizer.fit_on_texts(total)
@@ -77,8 +21,8 @@ x_train = tokenizer.texts_to_matrix(total, mode='freq')
 y_train = array([0 for _ in range(900)] + [1 for _ in range(900)])
 
 #Testing Set
-pos_reviews = loadReviews('data/pos', vocab, False)
-neg_reviews = loadReviews('data/neg', vocab, False)
+pos_reviews = util.loadReviews('data/pos', vocab, False)
+neg_reviews = util.loadReviews('data/neg', vocab, False)
 total = pos_reviews+neg_reviews
 x_test = tokenizer.texts_to_matrix(total, mode='freq')
 y_test = array([0 for _ in range(100)] + [1 for _ in range(100)])
@@ -99,7 +43,7 @@ print('Test data accuracy: %f' % (acc*100))
 
 # Sample reviews - EDIT THIS TO TEST MLP NETWORK (0 if positive, 1 if negative)
 text1 = "The first film, a frenetic and hilarious tribute to superhero films and imbued with 1950s style, was an absolute masterpiece, with great characters and hugely inventive action set pieces. This one is a worthy successor. That’s all you need to know"
-print(predictReview(text1, vocab, tokenizer, model))
+print(util.predictReview(text1, vocab, tokenizer, model))
 
 text2 = "I'm not usually a fan of suspenseful movies, but this one surprised me in a lot of ways. Of course, it's not without its shortcomings: the foil is pretty easy to spot early on, and the monster is fairly unimaginative, but there's a beauty in its simplicity, particularly in the family's story, that draws you in.John Krasinski does it all, providing a great performance and excellent direction of himself and his peers. Even though it's never one to subvert your expectations, the thrill of seeing it all unfold is very enjoyable. Though I didn't particularly enjoy the ending, I thought the rest of the film was strong enough to make up for it.What it does well: The quiet, honestly, is done perfectly. It's not abused with jump scares, and really feels as if it's its own character.What it could improve on:Itwill, almost certainly, leave you with some questions.Why didn't they just do this?was a common discussion topic among my group."
-print(predictReview(text2, vocab, tokenizer, model))
+print(util.predictReview(text2, vocab, tokenizer, model))
---
+++ BagOfWords/mlp/util.py
@@ -0,0 +1,56 @@
+import string
+from nltk.corpus import stopwords
+from string import punctuation
+from os import listdir
+from collections import Counter
+### Preprocessing Methods ###
+
+#Open file in read-only and extract content into variable 'content'
+def loadFile(filename):
+    openFile = open(filename, 'r')
+    content = openFile.read()
+    openFile.close()
+    return content
+
+#Tokenize file
+def tokenizeFile(filename):
+    tokens = filename.split()                                   #remove whitespace
+    tokens = [x.strip(string.punctuation) for x in tokens]           #remove punctuation
+    tokens = [word for word in tokens if word.isalpha()]        #remove none alphabetic words
+    stopWords = set(stopwords.words('english'))                 #remove stop words
+    tokens = [word for word in tokens if not word in stopWords]
+    tokens = [word for word in tokens if len(word) > 1]         #remove 1-letter tokens
+    return tokens
+
+#Convert tokens to single strings for easier encoding
+def fileToLine(filename, vocab):
+    content = loadFile(filename)
+    tokens = tokenizeFile(content)
+    tokens = [word for word in tokens if word in vocab]
+    return ' '.join(tokens)
+
+#Load all reviews and start mapping words to counter
+def loadReviews(directory, vocab, is_train):
+    lines = list()
+    for filename in listdir(directory):
+        if filename.startswith('cv9') and is_train:
+            continue
+        if not filename.startswith('cv9') and not is_train:
+            continue
+        path = directory + '/' + filename
+        line = fileToLine(path, vocab)
+        lines.append(line)
+    return lines
+
+#Predict reviews based on MLP network
+def predictReview(review, vocab, tokenizer, model):
+
+    #Split review into words and filter based on current vocab
+    tokens = tokenizeFile(review)
+    tokens = [word for word in tokens if word in vocab]
+    lines = ' '.join(tokens)
+    encode = tokenizer.texts_to_matrix([lines], mode='freq')
+
+    #Predict review: 0 if positive, 1 if negative
+    y = model.predict(encode, verbose=0)
+    return round(y[0,0])
